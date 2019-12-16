8925c6b1244d5039bbb8eed0c452b0803f0f0fca
sonet-sunet <slava.zhukov@gmail.com>
2019-12-16 20:52:42 +0300

    Fix Catalog From Bugs

--- parts/footer.php
+++ parts/footer.php
@@ -29,6 +29,7 @@
             </div>
         </div>
     </div>
+    <script src="/js/jquery-3.4.1.min.js"></script>
     <script src="/js/script.js"></script>
     <script src="/js/<?=$footer_config['page-js']?>"></script>
 </body>
